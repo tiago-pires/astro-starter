07efd61cb41eabe78faf1f903a2eed7df119e9fa
Tiago Pires <tiago.pires@me.com>
2024-04-17 18:06:15 +1000

    Add fa6 iconset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
@@ -10,6 +11,8 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     react(),
+    icon({
+      "fa6-brands": ["youtube", "instagram", "facebook"],
+    }),
   ],
 });
-
