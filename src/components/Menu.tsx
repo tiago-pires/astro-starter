2364c5f16c3cf426b41ed7fa685f1e77b796d02f
Tiago Pires <tiago.pires@me.com>
2024-04-17 19:16:27 +1000

    Add NavBar.astro

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
+import SocialLinks from "../components/SocialLinks";
 export default function MobileMenu() {
   return (
     <Sheet>
@@ -10,7 +11,7 @@ export default function MobileMenu() {
         </Button>
       </SheetTrigger>
       <SheetContent side="right">
-        <div className="p-8">(Menu...)</div>
+        <SocialLinks />
       </SheetContent>
     </Sheet>
   );
